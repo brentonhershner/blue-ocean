f617f9e172d8ab17b048be54ce55fa27d1fe2a7e
Jared Rogers <rogersjaredt@gmail.com>
2021-05-24 16:21:41 -0700

    fixed import statements; added endpoints

--- server/photosRouter.js
+++ server/photosRouter.js
@@ -1,8 +1,27 @@
-const express = require('express');
+import express from 'express';
 const photosRouter = express.Router();
 
 photosRouter.get('/allPhotos', (req, res) => {
 
 })
+
+
+photosRouter.patch('/single', (req, res) => {
+
+});
+
+photosRouter.patch('/multiple', (req, res) => {
+    //update specified information on specified photo(s)
+  
+  
+  });
+
+photosRouter.put('/', (req, res) => {
+    //upload a new photo
+    //this functionality is currently in images route, not sure how we want to handle upload. 
+    //maybe copy image upload over to here, but at somepoint we need to handle photo information and inserting that to db
+    //^^^Brenton has the upload stuff figured out, so may need to coordinate with him briefly
+})
+
 
 export default photosRouter;
--- server/usersRouter.js
+++ server/usersRouter.js
@@ -1,8 +1,19 @@
-const express = require('express');
+import express from 'express';
 const usersRouter = express.Router();
 
-usersRouter.get('/:userId', (req, res) => {
+usersRouter.put('/friends/:action', (req, res) => {
+    //action will be request a userId to be friend, cancelRequest to a userId, accept a request from a userId, reject a request from user id,
+    //or remove an existing friend
+    //So overall: 'request', 'cancelRequest', 'accept', 'reject', 'remove' will be different parameter routes
+});
 
+usersRouter.put('/', (req, res) => {
+    //create new user
+})
+
+usersRouter.delete('/', (req, res) => {
+    //this is future feature for superusers, providing option to delete a user account
+    //don't need to worry about it for Monday
 })
 
 export default usersRouter;
