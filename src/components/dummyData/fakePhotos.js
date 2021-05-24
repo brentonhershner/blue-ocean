14477c3b69277e0d8dfc9e6fb504bdb5e4509d90
Tony Ly <tonyjly@gmail.com>
2021-05-24 10:37:26 -0700

    moved dummyData from root to src/components to solve relative import

--- src/components/dummyData/fakePhotos.js
+++ src/components/dummyData/fakePhotos.js
@@ -1,6 +1,7 @@
 const fakePhotos =
 [
   {
+<<<<<<< HEAD:dummyData/fakePhotos.js
     ownerId: 2,
     ownerName: 'Scrub User',
     photos: [
@@ -68,3 +69,36 @@ const fakePhotos =
 ];
 
 export default fakePhotos;
+=======
+    photoId: 1,
+    ownerId: 1,
+    ownerName: 'DanDruff',
+    uploadDate: 'May 16 2021 18:11:26 GMT-0700',
+    description: 'Go go power rangers!',
+    tags: ['mmpr', 'coachella'],
+    permission: 2,
+    url: '/blue-ocean/dummyData/photos/photo-1.jpg',
+  }, {
+    photoId: 2,
+    ownerId: 1,
+    ownerName: 'DanDruff',
+    uploadDate: 'May 17 2021 18:11:26 GMT-0700',
+    description: 'man\'s best friend',
+    tags: ['bff4ever', 'coachella'],
+    permission: 0,
+    url: '/blue-ocean/dummyData/photos/photo-2.jpeg',
+  }, {
+    photoId: 3,
+    ownerId: 2,
+    ownerName: 'NunyoBiz',
+    uploadDate: 'May 19 2021 18:11:26 GMT-0700',
+    description: 'get on my level',
+    tags: [],
+    permission: 2,
+    url: '/blue-ocean/dummyData/photos/photo-3.jpg',
+  }
+
+]
+
+export default fakePhotos;
+>>>>>>> moved dummyData from root to src/components to solve relative import:src/components/dummyData/fakePhotos.js
