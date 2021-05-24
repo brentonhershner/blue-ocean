6b9a5ba5454a3d84892b6d718d70643fd26ac78b
Brenton Hershner <brenthershner@gmail.com>
2021-05-24 10:22:28 -0700

    changed the route

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography'
 import { Link as RouterLink } from 'react-router-dom';
+import Search from './navbar/Search';
+
+const Home = () => {
+  return (
     <div>
       <Search />
       <Typography variant="h2">
