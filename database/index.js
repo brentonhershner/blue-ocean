9bf4f347c7951978120024df13c9ac3045f9ef2d
Kyle Harrington <harrington.kyle@gmail.com>
2021-05-24 10:15:16 -0700

    create schemas for future database implementation

--- database/index.js
+++ database/index.js
@@ -1,4 +1,5 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
 mongoose.connect('mongodb://localhost/db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -6,12 +7,48 @@ mongoose.connect('mongodb://localhost/db', {
 
 const db = mongoose.connection;
 
-db.on('error', (error) =>{
+db.on('error', (error) => {
   console.error('mongoose connection error', error);
 });
 
 db.once('open', () => {
   console.info('mongoose connected successfully');
 });
+
+const friendSchema = new Schema({
+  userId: Number,
+  userName: String
+});
+//
+
+const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  userLevel: Number,  /* 1=general user, 2='super user', 3=admin  */
+  friends: [friendSchema],
+  pending: [friendSchema],
+  requested: [friendSchema]
+});
+
+const photoSchema = new Schema({
+  uploadDate: Date,
+  description: String,
+  tags: Array,
+  accessLevel: Number,  /* 0=private, [1=select friends(futureFeature)], 2=all friends, [3=global(futureFeature)] */
+  url: String
+});
+
+const userPhotosSchema = new Schema({
+  ownerId: Number,
+  ownerName: String,
+  photos: [photoSchema]
+});
+
+const Friend = model('Friend', friendSchema);
+const User = model('User', userSchema);
+const Photo = model('Photo', photoSchema);
+const UserPhotos = model('UserPhotos', userPhotosSchema);
 
 export default db;
