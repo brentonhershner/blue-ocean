da73da0e3c3f5d4f6d695e8f3112ac1772150382
Jun Park <jun.park06@gmail.com>
2021-05-25 14:31:47 -0700

    why tho

--- src/components/photoGallery/EditPhotosModal.js
+++ src/components/photoGallery/EditPhotosModal.js
@@ -8,11 +8,7 @@ import Button from '@material-ui/core/Button';
 // import IconButton from '@material-ui/core/IconButton';
 import Modal from '@material-ui/core/Modal';
 import InputLabel from '@material-ui/core/InputLabel';
-<<<<<<< HEAD
-//import Input from '@material-ui/core/Input';
-=======
 // import Input from '@material-ui/core/Input';
->>>>>>> added functionalities that were deleted by rebasing
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select';
