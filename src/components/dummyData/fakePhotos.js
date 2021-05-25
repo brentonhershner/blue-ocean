b4f1deac6f1a82b7827ee97ce67f52214a112771
Tony Ly <tonyjly@gmail.com>
2021-05-24 17:39:50 -0700

    move dummyData from root to src/components/dummyData

--- src/components/dummyData/fakePhotos.js
+++ src/components/dummyData/fakePhotos.js
@@ -1,6 +1,7 @@
 const fakePhotos =
 [
   {
+<<<<<<< HEAD
     ownerId: 2,
     ownerName: 'Scrub User',
     photos: [
@@ -66,5 +67,36 @@ const fakePhotos =
     ]
   }
 ];
+=======
+    photoId: 1,
+    ownerId: 1,
+    ownerName: 'DanDruff',
+    uploadDate: 'May 16 2021 18:11:26 GMT-0700',
+    description: 'Go go power rangers!',
+    tags: ['mmpr', 'coachella'],
+    permission: 2,
+    url: './photos/photo-1.jpg',
+  }, {
+    photoId: 2,
+    ownerId: 1,
+    ownerName: 'DanDruff',
+    uploadDate: 'May 17 2021 18:11:26 GMT-0700',
+    description: 'man\'s best friend',
+    tags: ['bff4ever', 'coachella'],
+    permission: 0,
+    url: './photos/photo-2.jpeg',
+  }, {
+    photoId: 3,
+    ownerId: 2,
+    ownerName: 'NunyoBiz',
+    uploadDate: 'May 19 2021 18:11:26 GMT-0700',
+    description: 'get on my level',
+    tags: [],
+    permission: 2,
+    url: './photos/photo-3.jpg',
+  }
+
+]
+>>>>>>> move dummyData from root to src/components/dummyData
 
 export default fakePhotos;
