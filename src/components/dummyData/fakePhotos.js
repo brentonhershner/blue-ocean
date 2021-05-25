458d7197b2ff14ff052c23ea4a3348847db60838
Tony Ly <tonyjly@gmail.com>
2021-05-25 00:54:28 -0700

    change to relative path for fakePhotos

--- src/components/dummyData/fakePhotos.js
+++ src/components/dummyData/fakePhotos.js
@@ -9,7 +9,7 @@ const fakePhotos = [
         description: 'Go go power rangers!',
         tags: ['mmpr', 'coachella'],
         accessLevel: 2,
-        url: '/blue-ocean/dummyData/photos/photo-1.jpg'
+        url: './photos/photo-1.jpg'
       },
       {
         photoId: 2,
@@ -17,21 +17,21 @@ const fakePhotos = [
         description: 'man\'s best friend',
         tags: ['bff4ever', 'coachella'],
         accessLevel: 0,
-        url: '/blue-ocean/dummyData/photos/photo-2.jpeg'
+        url: './photos/photo-2.jpeg'
       },
       {
         uploadDate: 'May 19 2021 18:11:26 GMT-0700',
         description: 'get on my level',
         tags: [],
         accessLevel: 2,
-        url: '/blue-ocean/dummyData/photos/photo-3.jpg'
+        url: './photos/photo-3.jpg'
       },
       {
         uploadDate: 'May 19 2021 18:11:26 GMT-0700',
         description: 'you can never have too much TP',
         tags: ['t-p'],
         accessLevel: 2,
-        url: '/blue-ocean/dummyData/photos/photo-7.jpg'
+        url: './photos/photo-7.jpg'
       }
     ]
   },
@@ -45,7 +45,7 @@ const fakePhotos = [
         description: 'my little teapot!',
         tags: ['mmpr', 'coachella', 't-pots'],
         accessLevel: 2,
-        url: '/blue-ocean/dummyData/photos/photo-4.jpg'
+        url: './photos/photo-4.jpg'
       },
       {
         photoId: 2,
@@ -53,14 +53,14 @@ const fakePhotos = [
         description: 't-pain is my bestie',
         tags: ['bff4ever', 'coachella', 't-pain'],
         accessLevel: 2,
-        url: '/blue-ocean/dummyData/photos/photo-5.jpeg'
+        url: './photos/photo-5.jpeg'
       },
       {
         uploadDate: 'May 19 2021 18:11:26 GMT-0700',
         description: 'about to tee-off',
         tags: ['t-time', 'mmpr'],
         accessLevel: 0,
-        url: '/blue-ocean/dummyData/photos/photo-6.jpg'
+        url: './photos/photo-6.jpg'
       }
     ]
   }
