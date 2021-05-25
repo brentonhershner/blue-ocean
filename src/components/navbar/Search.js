e5a605a79a7e28c211026a9786537a35beb650af
Tony Ly <tonyjly@gmail.com>
2021-05-24 23:49:51 -0700

    import photos context, add searchTerm state

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 // import Link from '@material-ui/core/Link';
 // import Typography from '@material-ui/core/Typography'
 // import { Link as RouterLink } from 'react-router-dom';
@@ -7,13 +7,15 @@ import React, { useState } from 'react';
 import SearchIcon from '@material-ui/icons/Search';
 import InputBase from '@material-ui/core/InputBase';
 import fakePhotos from '../dummyData/fakePhotos';
+import { PhotosContext } from '../../contexts/photos-context.js';
 
 // Two options for Search field:
 // 1. App Bar:  https://material-ui.com/components/app-bar/#app-bar-with-a-primary-search-field
 // 2. Free Solo (currently using):  https://material-ui.com/components/autocomplete/#search-input
 
 const Search = () => {
-  const [photoList, setPhotoList] = useState(fakePhotos)
+  const [ searchTerm, setSearchTerm ] = useState(null);
+  const { photos, setPhotos, updatePhoto } = useContext(PhotosContext);
 
   return (
     <div>
@@ -21,6 +23,7 @@ const Search = () => {
         data-testid="search-bar"
         placeholder="Search…"
         inputProps={{ 'aria-label': 'search' }}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <SearchIcon />
     </div>
