ee93b1628ed51bc1967ab457ad3730a6481402f6
Tony Ly <tonyjly@gmail.com>
2021-05-25 01:05:52 -0700

    add search logic to filter results, generate new array of search results

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -1,13 +1,28 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect /*, useContext */ } from 'react';
 import SearchIcon from '@material-ui/icons/Search';
 import InputBase from '@material-ui/core/InputBase';
-// import fakePhotos from '../dummyData/fakePhotos';
-import { PhotosContext } from '../../contexts/photos-context.js';
+// import { PhotosContext } from '../../contexts/photos-context.js';
+import fakePhotos from '../dummyData/fakePhotos';
+
+// SEARCH BY
+// 1. tags
+// 2. users/friends
 
 const Search = () => {
   const [ searchTerm, setSearchTerm ] = useState(null);
-  const [ searchResult, setSearchResult ] = useState([]);
-  const { photos, setPhotos, updatePhoto } = useContext(PhotosContext);
+  const [ searchResults, setSearchResults ] = useState([]);
+  // const { photos } = useContext(PhotosContext); // enable when `photos-context` is ready to use
+
+  useEffect(() => {
+    // upon `searchResults` change, send results to render view of new array
+  }, [searchResults])
+
+  function search() {
+    const matches = fakePhotos[0].photos.filter(photo => photo.tags.includes(searchTerm.toLowerCase()))
+    setSearchResults(matches)
+  }
+
+
 
   return (
     <div>
