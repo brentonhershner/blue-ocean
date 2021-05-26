24ff0e5ca7ff932ccc35d2f6b4c96038c8ce7f60
GTOnizuka13 <darkrain0123@gmail.com>
2021-05-25 20:21:51 -0700

    Friends list complete

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -15,35 +15,64 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function renderRow(props) {
-  const { index, style}  = props;
-  console.log(style, "this is style ")
+function renderRow(friends, pending, requested) {
+
   return (
-    <ListItem button style={style} key={index}>
-      {/* {props ? props.map((friends) => ( <ListItemText  primary={friends.username} key={ friends.userId } /> )) : null} */}
-       <ListItemText primary={`Item ${props}`} />
+    <div>
+
+{/* Friends on your friends list  */}
+    {friends.map((item, i) => (
+      <ListItem button>
+    <ListItemText primary={`${item.username}`} secondary={'Friend'} key={i}/>
+
     </ListItem>
-  );
+    ))
+  }
+  {/* Requested friends  */}
+  {pending.map((item, i) => (
+      <ListItem button>
+    <ListItemText primary={`${item.username}`} secondary={'Pending'} key={i}/>
+
+    </ListItem>
+    ))
+  }
+{/* Friends you have requested to be friends with  */}
+{requested.map((item, i) => (
+      <ListItem button>
+    <ListItemText primary={`${item.username}`} secondary={'Requested'} key={i}/>
+
+    </ListItem>
+    ))
+  }
+    </div>
+  )
+
+  // const { index, style}  = props;
+  // console.log(style, "this is style ")
+  // return (
+  //   <ListItem button style={style} key={index}>
+  //     {/* {props ? props.map((friends) => ( <ListItemText  primary={friends.username} key={ friends.userId } /> )) : null} */}
+  //      <ListItemText primary={`Item ${props}`} />
+  //   </ListItem>
+  // );
 }
 
-renderRow.propTypes = {
-  index: PropTypes.number.isRequired,
-  style: PropTypes.object.isRequired,
-};
+// renderRow.propTypes = {
+  //   index: PropTypes.number.isRequired,
+  //   style: PropTypes.object.isRequired,
+  // };
 
-function FriendsList({friends, pending, requested, index, style }) {
-  const classes = useStyles();
+  function FriendsList({friends, pending, requested, index, style }) {
+    const classes = useStyles();
 
   // const { userName, friends } = useContext(UserContext)
   console.log(friends)
   return (
     <div className={classes.root}>
-      {friends.map((item, i) => (
-        <ListItem  >
-        <ListItemText primary={`${item.username}`} key={i}></ListItemText>
-        </ListItem>
-        ))}
-
+      <p>Friends</p>
+      <FixedSizeList height={300} width={300} itemSize={36} itemCount={1} >
+{() => renderRow(friends, pending, requested) }
+        </FixedSizeList>
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import GoogleLogin from 'react-google-login';
-import axios from 'axios';
+// import React from 'react';
+// import GoogleLogin from 'react-google-login';
+// import axios from 'axios';
 
-const Login = () => {
-  const responseGoogle = (response) => {
-    console.log(response);
-    console.log(response.profileObj)
-  }
-  return (
-    <div>
-      <GoogleLogin
-        clientId="652150268889-1ropi2okc0kble20c913qi5lk88krb4t.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
-    </div>
-  )
-}
+// const Login = () => {
+//   const responseGoogle = (response) => {
+//     console.log(response);
+//     console.log(response.profileObj)
+//   }
+//   return (
+//     <div>
+//       <GoogleLogin
+//         clientId="652150268889-1ropi2okc0kble20c913qi5lk88krb4t.apps.googleusercontent.com"
+//         buttonText="Login"
+//         onSuccess={responseGoogle}
+//         onFailure={responseGoogle}
+//         cookiePolicy={'single_host_origin'}
+//       />
+//     </div>
+//   )
+// }
 
-export default Login;
+// export default Login;
